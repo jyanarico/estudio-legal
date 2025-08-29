447581a63722a118dc828fd88da57bc2b511ced2
jyanarico <alexispps3@gmail.com>
2025-08-29 18:47:48 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
         theme_color: '#293C3D',
         background_color: '#293C3D',
         display: 'standalone',
-        start_url: '/consultas',
+        start_url: '/',
         icons: [
           { src: '/icons/icon-192.png', sizes: '192x192', type: 'image/png' },
           { src: '/icons/icon-512.png', sizes: '512x512', type: 'image/png' },
